749da012b0e928ecd7cc85bfc72c7d044c8adf93
Daivy Morales <daivymorales7705@gmail.com>
2023-08-31 09:48:49 -0500

    feat(form): initial base form

---
+++ src/pages/register/index.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { useFormik } from "formik";
+
+export default function Register() {
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <form
+        onSubmit={formik.handleSubmit}
+        className="bg-black text-white flex flex-col p-3"
+      >
+        <label htmlFor="name">Name</label>
+        <input
+          id="name"
+          name="name"
+          type="text"
+          onChange={formik.handleChange}
+          value={formik.values.name}
+        />
+        <label htmlFor="email">Email</label>
+        <input
+          id="email"
+          name="email"
+          type="email"
+          onChange={formik.handleChange}
+          value={formik.values.email}
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          id="password"
+          name="password"
+          type="password"
+          onChange={formik.handleChange}
+          value={formik.values.password}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
