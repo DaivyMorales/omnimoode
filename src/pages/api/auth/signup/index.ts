a611772f39044728f1a12956d80f5f07cd151f47
Daivy Morales <daivymorales7705@gmail.com>
2023-08-31 07:49:53 -0500

    chore(auth): next-auth bcryptjs axios

--- src/pages/api/auth/signup/index.ts
+++ src/pages/api/auth/signup/index.ts
@@ -1,7 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 import { PrismaClient } from "@prisma/client";
-import { User } from "@/types/user";
 
 const prisma = new PrismaClient();
 
