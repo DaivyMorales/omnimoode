0d9ce75224b103a5308d081c75d1e8628571066a
Daivy Morales <daivymorales7705@gmail.com>
2023-10-08 17:32:16 -0500

    feat(searchBar): button close

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { AiOutlineSearch } from 'react-icons/ai';
-import { PiTShirtBold, PiPantsBold, PiCoatHangerBold } from 'react-icons/pi';
+import { PiXBold } from 'react-icons/pi';
 import { useAppDispach, useAppSelector } from '@/redux/hooks'
 import { useGetCategoriesQuery } from '@/redux/api/cotegoryApi'
 import CategoryCard from './Category/CategoryCard';
@@ -25,24 +25,27 @@ export default function SearchBar({ openSearch, setOpenSearch }: SearchBarProps)
       className="absolute w-full h-full flex justify-center items-start"
       style={{ backdropFilter: 'blur(5px) brightness(80%)' }}
     >
-      <div className="container sm:px-10 lg:px-30 xl:px-52 p-2 flex flex-col justify-center items-center md:mt-20 mt-5">
-        <div className="w-full bg-white ring-1 ring-gray-200 rounded-t-md shadow-lg text-gray-400 px-3 py-2 flex justify-start items-center gap-3 cursor-pointer text-xl hover:ring-gray-300 hover:text-gray-500">
-          <div>
-            <AiOutlineSearch size={20} />
+      <div className="container sm:px-10 lg:px-30 xl:px-52 p-2 flex gap-2 justify-center items-start md:mt-20 mt-5">
+        <div className='w-full'>
+          <div className="w-full bg-white ring-1 ring-gray-200 rounded-t-md shadow-lg text-gray-400 px-3 py-2 flex justify-start items-center gap-3 cursor-pointer text-xl hover:ring-gray-300 hover:text-gray-500">
+            <div>
+              <AiOutlineSearch size={20} />
+            </div>
+            <input type="text" className="text-lg text-black" placeholder="¿Qué estas buscando?" />
           </div>
-          <input type="text" className="text-lg text-black" placeholder="¿Qué estas buscando?" />
-        </div>
-        <div className='bg-white ring-1 ring-gray-200 w-full rounded-b-md flex flex-col justify-start gap-y-1 p-3'>
+          <div className='bg-white ring-1 ring-gray-200 w-full rounded-b-md flex flex-col justify-start gap-y-1 p-3'>
 
-          <h3 className='text-gray-600'>Categorias</h3>
-          {
-            error ? (<div>Hubo un error</div>) : (
-              data?.map((category) => (
-                <CategoryCard category={category} key={category.id} />
-              ))
-            )
-          }
+            <h3 className='text-gray-600'>Categorias</h3>
+            {
+              error ? (<div>Hubo un error</div>) : (
+                data?.map((category) => (
+                  <CategoryCard category={category} key={category.id} />
+                ))
+              )
+            }
+          </div>
         </div>
+        <div className='bg-white ring-1 ring-gray-200 cursor-pointer p-2 rounded-md hover:bg-gray-300' onClick={() => setOpenSearch(false)}><PiXBold /></div>
       </div>
     </div>
   );
