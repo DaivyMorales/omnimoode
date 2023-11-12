1058daae350be458c7b31f8ca88a578b7e8e5d2f
Daivy Morales <daivymorales7705@gmail.com>
2023-11-12 08:23:41 -0500

    fix: type Cart on .map

--- src/components/Cart/ProductCard.tsx
+++ src/components/Cart/ProductCard.tsx
@@ -1,11 +1,11 @@
-import { useEffect, useState, ChangeEvent } from 'react';
-import Image from 'next/image';
-import { CartProduct, ProductCalculated } from '@/types/index';
-import { PiTrashBold } from 'react-icons/pi';
-import { setCart, deleteCartProduct } from '@/redux/features/cartSlice';
-import { useAppSelector } from '@/redux/hooks';
-import { useDispatch } from 'react-redux';
-import axios from 'axios';
+import { useEffect, useState, ChangeEvent } from "react";
+import Image from "next/image";
+import { Cart, CartProduct, ProductCalculated } from "@/types/index";
+import { PiTrashBold } from "react-icons/pi";
+import { setCart, deleteCartProduct } from "@/redux/features/cartSlice";
+import { useAppSelector } from "@/redux/hooks";
+import { useDispatch } from "react-redux";
+import axios from "axios";
 
 interface MyProps {
   cartProduct: CartProduct;
@@ -36,7 +36,7 @@ export default function ProductCard({
   const remove = async () => {
     await dispatch(deleteCartProduct(cartProduct.id) as any);
 
-    dispatch(setCart(cart.filter((c) => c.id !== cartProduct.id)));
+    dispatch(setCart(cart.filter((c: Cart) => c.id !== cartProduct.id)));
   };
 
   const updateCartProductQuantity = async (number: number) => {
@@ -55,7 +55,7 @@ export default function ProductCard({
   const handleQuantityChange = (e: ChangeEvent<HTMLInputElement>) => {
     const inputText = e.target.value;
 
-    if (inputText === '') {
+    if (inputText === "") {
       setAllInputsEmpty(true);
     } else {
       const numberSelected = Number(inputText);
@@ -76,34 +76,34 @@ export default function ProductCard({
   const calculateFinalPrice = price * quantity;
 
   return (
-    <tr className=' w-full'>
-      <td className='w-1/5 '>
+    <tr className=" w-full">
+      <td className="w-1/5 ">
         <Image
           src={cartProduct.product.imageUrl}
           width={50}
           height={50}
-          alt='Product Image'
-          style={{ width: 'auto', height: 'auto' }}
-          className='border-1 rounded-md'
+          alt="Product Image"
+          style={{ width: "auto", height: "auto" }}
+          className="border-1 rounded-md"
         />
       </td>
-      <td className='w-2/5 '>
-        <p className='text-sm font-medium'>{cartProduct.product.name}</p>
-        <p className='text-gray-600'>
-          Hoddie <span className='text-gray-300'>|</span> Blanco{' '}
-          <span className='text-gray-300'>| </span>
-          {cartProduct.size.name.toUpperCase()}{' '}
+      <td className="w-2/5 ">
+        <p className="text-sm font-medium">{cartProduct.product.name}</p>
+        <p className="text-gray-600">
+          Hoddie <span className="text-gray-300">|</span> Blanco{" "}
+          <span className="text-gray-300">| </span>
+          {cartProduct.size.name.toUpperCase()}{" "}
         </p>
       </td>
 
-      <td className='w-1/5'>
-        <div className='border-1 rounded-md flex justify-between items-center px-1 -py-1'>
+      <td className="w-1/5">
+        <div className="border-1 rounded-md flex justify-between items-center px-1 -py-1">
           <button
             disabled={quantity < 1}
             className={`text-lg font-normal ${
               quantity === 1
-                ? 'text-gray-200 cursor-not-allowed'
-                : 'text-gray-500'
+                ? "text-gray-200 cursor-not-allowed"
+                : "text-gray-500"
             }`}
             onClick={async () => {
               if (quantity > 1) {
@@ -120,14 +120,14 @@ export default function ProductCard({
             -
           </button>
           <input
-            type='number'
-            className='w-6 text-center font-semibold text-xs text-gray-800 inputSizeCart'
-            value={quantity === 0 ? '' : quantity}
+            type="number"
+            className="w-6 text-center font-semibold text-xs text-gray-800 inputSizeCart"
+            value={quantity === 0 ? "" : quantity}
             onChange={handleQuantityChange}
           />
 
           <button
-            className='text-lg font-normal text-gray-500'
+            className="text-lg font-normal text-gray-500"
             onClick={async () => {
               const sumQuantity = quantity + 1;
               setquantity(sumQuantity);
@@ -140,21 +140,21 @@ export default function ProductCard({
           </button>
         </div>
       </td>
-      <td className='px-2 w-full '>
-        <p className='text-sm font-medium'>
+      <td className="px-2 w-full ">
+        <p className="text-sm font-medium">
           ${calculateFinalPrice.toLocaleString()}
         </p>
       </td>
-      <td className='w-full '>
+      <td className="w-full ">
         <div
           onMouseEnter={() => setHoverDelete(true)}
           onMouseLeave={() => setHoverDelete(false)}
           onClick={async () => remove()}
-          className='flex justify-end cursor-pointer'
+          className="flex justify-end cursor-pointer"
         >
           <PiTrashBold
             size={17}
-            color={`${hoverDelete ? '#ef4444' : '#9ca3af'}`}
+            color={`${hoverDelete ? "#ef4444" : "#9ca3af"}`}
           />
         </div>
       </td>
