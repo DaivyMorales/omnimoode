70aabf9b8860f25dcd4fd798f9f18df5429b138b
Daivy Morales <daivymorales7705@gmail.com>
2023-11-24 08:18:18 -0500

    feat: address Api global state by id

---
+++ src/redux/api/addressApi.ts
@@ -0,0 +1,17 @@
+import { Address } from '@/types';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const addressApi = createApi({
+  reducerPath: 'addressApi',
+  refetchOnFocus: true,
+  baseQuery: fetchBaseQuery({
+    baseUrl: '/api/',
+  }),
+  endpoints: (builder) => ({
+    getAddressById: builder.query<Address, { id: number }>({
+      query: ({ id }) => `address/${id}`,
+    }),
+  }),
+});
+
+export const { useGetAddressByIdQuery } = addressApi;
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,14 +1,15 @@
-import { configureStore } from "@reduxjs/toolkit";
-import NumberValidation from "./features/NumberValidationSlice";
-import EmailRecoveryPassword from "./features/EmailRecoveryPasswordSlice";
-import cartSlice from "./features/cartSlice";
-import priceFinalSlice from "./features/priceFinalSlice";
-import { setupListeners } from "@reduxjs/toolkit/dist/query";
+import { configureStore } from '@reduxjs/toolkit';
+import NumberValidation from './features/NumberValidationSlice';
+import EmailRecoveryPassword from './features/EmailRecoveryPasswordSlice';
+import cartSlice from './features/cartSlice';
+import priceFinalSlice from './features/priceFinalSlice';
+import { setupListeners } from '@reduxjs/toolkit/dist/query';
 
-import { categoryApi } from "./api/cotegoryApi";
-import { productApi } from "./api/productApi";
-import { cartApi } from "./api/cartApi";
-import { newestProductApi } from "./api/newestProductApi";
+import { categoryApi } from './api/cotegoryApi';
+import { productApi } from './api/productApi';
+import { cartApi } from './api/cartApi';
+import { newestProductApi } from './api/newestProductApi';
+import { addressApi } from './api/addressApi';
 
 export const store: any = configureStore({
   reducer: {
@@ -20,14 +21,16 @@ export const store: any = configureStore({
     [productApi.reducerPath]: productApi.reducer,
     [cartApi.reducerPath]: cartApi.reducer,
     [newestProductApi.reducerPath]: newestProductApi.reducer,
+    [addressApi.reducerPath]: addressApi.reducer,
   },
-  devTools: process.env.NODE_ENV !== "production",
+  devTools: process.env.NODE_ENV !== 'production',
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat([
       categoryApi.middleware,
       productApi.middleware,
       cartApi.middleware,
       newestProductApi.middleware,
+      addressApi.middleware,
     ]),
 });
 
