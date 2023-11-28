7f5bebcdd521395deff8e9605db2c92d81d76db0
Daivy Morales <daivymorales7705@gmail.com>
2023-11-28 16:36:17 -0500

    fix: comment line email

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -9,17 +9,18 @@ import { HiBadgeCheck, HiSupport, HiTrash } from 'react-icons/hi';
 import { useGetAddressByIdQuery } from '@/redux/api/addressApi';
 import { Address, Card } from '@/types';
 import { useGetCardByIdQuery } from '@/redux/api/cardApi';
-import { setShowAlertAddress } from '@/redux/features/showAlertAddressSlice';
+import { setShowAddress, setShowCard } from '@/redux/features/showAlertsSlice';
 import { setAddresses } from '@/redux/features/addressSlice';
+import { setCards } from '@/redux/features/cardSlice';
 
 export default function ProfilePage() {
   const { data: session } = useSession();
 
   const [onHoverAddress, setOnHoverAddress] = useState(false);
-  const [onHoverCard, setOnHoverCard] = useState(false);
+  const [onHoverCard, setOnHoverCard] = useState(0);
 
   const addresses = useAppSelector((state) => state.addressSlice.addresses);
-  const [cards, setCards] = useState<any>([]);
+  const cards = useAppSelector((state) => state.cardSlice.cards);
 
   const dispach = useAppDispach();
 
@@ -29,16 +30,15 @@ export default function ProfilePage() {
     id: userId,
   });
 
+  console.log(addresses?.length);
+
   const { data: dataCard } = useGetCardByIdQuery({
     id: userId,
   });
 
-  useEffect(() => {
-    if (dataAddress && dataCard) {
-      dispach(setAddresses(dataAddress));
-      setCards(dataCard);
-    }
-  }, [dataCard, dataAddress]);
+  dispach(setAddresses(dataAddress));
+  dispach(setCards(dataCard));
+  useEffect(() => {}, []);
 
   return (
     <div className='relative h-full py-24 w-full flex flex-col items-center justify-center -mt-20'>
@@ -58,7 +58,7 @@ export default function ProfilePage() {
               <div className='flex items-center gap-1'>
                 <h3>{session?.user?.name}</h3>
 
-                {session?.user?.email_verification && <HiBadgeCheck />}
+                {/* {session?.user?.email_verification && <HiBadgeCheck />} */}
               </div>
               <h4 className='text-gray-500'>{session?.user?.email}</h4>
             </div>
@@ -72,7 +72,7 @@ export default function ProfilePage() {
         <div className='flex flex-col  justify-center  items-center border-1 w-full p-5 rounded-lg'>
           <div className='flex flex-col justify-start items-start gap-4 w-full'>
             <h3>Tus domicilios</h3>
-            {addresses > 0 ? (
+            {addresses?.length > 0 ? (
               addresses?.map((address: Address) => (
                 <div
                   key={address.id}
@@ -82,7 +82,7 @@ export default function ProfilePage() {
                 >
                   {onHoverAddress && (
                     <div
-                      onClick={() => dispach(setShowAlertAddress(true))}
+                      onClick={() => dispach(setShowAddress(true))}
                       className='-top-2 -right-2 absolute p-1 border-1 rounded-full bg-white border-black cursor-pointer'
                     >
                       <HiTrash />
@@ -142,48 +142,61 @@ export default function ProfilePage() {
         <div className='flex flex-col  justify-center  items-center border-1 w-full p-5 rounded-lg'>
           <div className='flex flex-col justify-start items-start gap-4 w-full'>
             <h3>Tus tarjetas</h3>
-            {cards?.map((card: Card) => (
-              <div
-                key={card.id}
-                onMouseEnter={() => setOnHoverCard(true)}
-                onMouseLeave={() => setOnHoverCard(false)}
-                className='relative flex justify-between items-center  w-full gap-y-3 border-1 rounded-lg p-3 hover:border-black'
-              >
-                {onHoverCard && (
-                  <div className='-top-2 -right-2 absolute p-1 border-1 rounded-full bg-white border-black cursor-pointer'>
-                    <HiTrash />
+            {cards?.length > 0 ? (
+              cards?.map((card: Card) => (
+                <div
+                  key={card.id}
+                  onMouseEnter={() => setOnHoverCard(card.id)}
+                  onMouseLeave={() => setOnHoverCard(card.id)}
+                  className='relative flex justify-between items-center  w-full gap-y-3 border-1 rounded-lg p-3 hover:border-black'
+                >
+                  {onHoverCard === card.id && (    
+                    <div
+                      onClick={() => {
+                        dispach(setShowCard(card.id));
+                      }}
+                      className='-top-2 -right-2 absolute p-1 border-1 rounded-full bg-white border-black cursor-pointer'
+                    >
+                      <HiTrash />
+                    </div>
+                  )}
+                  <div className='grid grid-cols-2  w-96'>
+                    <div>
+                      <label className='label-profile'>Número de tarjeta</label>
+                      <p className='font-semibold'>
+                        {card.card_number
+                          .replace(/.(?=.{4})/g, '*')
+                          .replace(/\s/g, '')
+                          .replace(/(.{4})/g, '$1')}
+                      </p>
+                    </div>
+                    <div>
+                      <label className='label-profile'>Nombres</label>
+                      <p className='font-semibold'>{card.names}</p>
+                    </div>
+                    <div>
+                      <label className='label-profile'>Apellidos</label>
+                      <p className='font-semibold'>{card.surnames}</p>
+                    </div>
+                    <div>
+                      <label className='label-profile'>Cedula</label>
+                      <p className='font-semibold'>
+                        {card.number_identification}
+                      </p>
+                    </div>
                   </div>
-                )}
-                <div className='grid grid-cols-2  w-96'>
-                  <div>
-                    <label className='label-profile'>Número de tarjeta</label>
-                    <p className='font-semibold'>
-                      {card.card_number
-                        .replace(/.(?=.{4})/g, '*')
-                        .replace(/\s/g, '')
-                        .replace(/(.{4})/g, '$1')}
-                    </p>
-                  </div>
-                  <div>
-                    <label className='label-profile'>Nombres</label>
-                    <p className='font-semibold'>{card.names}</p>
-                  </div>
-                  <div>
-                    <label className='label-profile'>Apellidos</label>
-                    <p className='font-semibold'>{card.surnames}</p>
-                  </div>
-                  <div>
-                    <label className='label-profile'>Cedula</label>
-                    <p className='font-semibold'>
-                      {card.number_identification}
-                    </p>
-                  </div>
+                  <button className=' text-[12px] bg-white border-1 text-[#666666] px-[6px] py-[5px] rounded-md'>
+                    Editar
+                  </button>
                 </div>
-                <button className=' text-[12px] bg-white border-1 text-[#666666] px-[6px] py-[5px] rounded-md'>
-                  Editar
-                </button>
+              ))
+            ) : (
+              <div>
+                <h3 className='text-[13px] text-gray-400'>
+                  No tienes ningúna tarjeta aún
+                </h3>
               </div>
-            ))}
+            )}
             <div className='w-full  flex justify-end'>
               <button
                 type='submit'
